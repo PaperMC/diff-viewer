ecd1982684674062fdcd735c8a3837a7f7a74a6d
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2025-06-18 15:58:39 -0400

    Add tests

--- web/src/test/util.test.ts
+++ web/src/test/util.test.ts
@@ -1,6 +1,25 @@
 import { expect, test } from "vitest";
+import {splitMultiFilePatch} from "$lib/util";
+import * as path from "node:path";
+import * as fs from "node:fs";
 
-// TODO add real test
-test("adds 1 + 2 to equal 3", () => {
-    expect(1 + 2).toBe(3);
+test("Yield 2 patches from a patch file with signature", () => {
+    const patch = loadPatch('patch-with-signature.patch');
+    expect(splitMultiFilePatch(patch).length).toBe(2);
 });
+
+
+test("Yield 2 patches from a patch file without signature", () => {
+    const patch = loadPatch('patch-without-signature.patch');
+    expect(splitMultiFilePatch(patch).length).toBe(2);
+});
+
+
+export function loadPatch(name: string): string {
+    const filePath = path.resolve(__dirname, "patches", name);
+
+    return fs.readFileSync(
+        filePath,
+        'utf8'
+    );
+}
