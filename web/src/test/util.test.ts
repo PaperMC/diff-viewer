e836ad7448dda2308c3ed5855cfb1492ab44adb7
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2025-06-18 16:00:19 -0400

    Fix

--- web/src/test/util.test.ts
+++ web/src/test/util.test.ts
@@ -1,25 +1,20 @@
 import { expect, test } from "vitest";
-import {splitMultiFilePatch} from "$lib/util";
+import { splitMultiFilePatch } from "$lib/util";
 import * as path from "node:path";
 import * as fs from "node:fs";
 
 test("Yield 2 patches from a patch file with signature", () => {
-    const patch = loadPatch('patch-with-signature.patch');
+    const patch = loadPatch("patch-with-signature.patch");
     expect(splitMultiFilePatch(patch).length).toBe(2);
 });
-
 
 test("Yield 2 patches from a patch file without signature", () => {
-    const patch = loadPatch('patch-without-signature.patch');
+    const patch = loadPatch("patch-without-signature.patch");
     expect(splitMultiFilePatch(patch).length).toBe(2);
 });
-
 
 export function loadPatch(name: string): string {
     const filePath = path.resolve(__dirname, "patches", name);
 
-    return fs.readFileSync(
-        filePath,
-        'utf8'
-    );
+    return fs.readFileSync(filePath, "utf8");
 }
