5c195f8955b1695c57a395f23e9535f26a63ae99
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-06-18 12:44:26 -0700

    Setup tests

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(() => ({
     plugins: [tailwindcss(), sveltekit()],
+    // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+    resolve: process.env.VITEST
+        ? {
+              conditions: ["browser"],
+          }
+        : undefined,
 }));
