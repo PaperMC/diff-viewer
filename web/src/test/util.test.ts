7acb5eb422bbfc6d67a159dae6e9121001a9be66
Owen <23108066+Owen1212055@users.noreply.github.com>
2025-06-30 19:45:23 -0400

    Make sure there is no space after the signature (#17)
    Still not perfect, but will do until this is rewritten to not use a regex.

--- web/src/test/util.test.ts
+++ web/src/test/util.test.ts
@@ -8,6 +8,11 @@ test("Yield 2 patches from a patch file with signature", () => {
     expect(splitMultiFilePatch(patch).length).toBe(2);
 });
 
+test("Yield 1 patches from a patch file with signature and double -- in the diff", () => {
+    const patch = loadPatch("patch-with-multiple-minus-diff-with-signature.patch");
+    expect(splitMultiFilePatch(patch).length).toBe(1);
+});
+
 test("Yield 2 patches from a patch file without signature", () => {
     const patch = loadPatch("patch-without-signature.patch");
     expect(splitMultiFilePatch(patch).length).toBe(2);
